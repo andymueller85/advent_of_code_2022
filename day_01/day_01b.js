411988d181086a6a2656524b9c2ed82b62abf9d2
andymueller85 <andymueller85@gmail.com>
2021-12-02 21:52:58 -0500

    cleanup

--- day_01/day_01b.js
+++ day_01/day_01b.js
@@ -5,7 +5,6 @@ const input = require('fs')
   .map(d => parseInt(d, 10))
 
   const count = input.reduce(
-    // can omit cur + a[i + 1] because both sides of comparison have that.
     (acc, cur, i, a) => (i > a.length - 4 || a[i + 3] <= cur ? acc : acc + 1),
     0
   )
