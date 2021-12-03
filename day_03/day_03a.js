f411c0cf553900c7c7ab7e5d7833cb74c937f1a0
Andy Mueller <andymueller85@gmail.com>
2021-12-03 17:02:08 -0500

    day 3a

---
+++ day_03/day_03a.js
@@ -0,0 +1,26 @@
+const input = require('fs')
+  .readFileSync('./day_03/input.txt', 'utf8')
+  .split('\n')
+  .filter(d => d)
+
+const gammaRate = Array.from({ length: input[0].length }).reduce(
+  (acc, _, i) =>
+    `${acc}${
+      input.map(n => n.split('')[i]).filter(d => d === '1').length >
+      input.length / 2
+        ? '1'
+        : '0'
+    }`,
+  ''
+)
+
+const epsilonRate = gammaRate
+  .split('')
+  .map(d => (d === '0' ? '1' : '0'))
+  .join('')
+
+const gammaAsDecimal = parseInt(gammaRate, 2)
+const epsilonAsDecimal = parseInt(epsilonRate, 2)
+
+console.log({ gammaRate, gammaAsDecimal, epsilonRate, epsilonAsDecimal })
+console.log({ answer: gammaAsDecimal * epsilonAsDecimal })
