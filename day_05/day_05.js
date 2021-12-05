a8175f0ca5ab33f7131c489f1da160a72a6efd78
andymueller85 <andymueller85@gmail.com>
2021-12-05 18:10:22 -0500

    day 5 cleanup

--- day_05/day_05.js
+++ day_05/day_05.js
@@ -1,65 +1,58 @@
-const go = (fileName, includeDiagonals) => {
+const mapHydrothermalVents = (fileName, gridSize, includeDiagonals) => {
   const input = require('fs')
     .readFileSync(fileName, 'utf8')
     .split('\n')
     .filter(d => d)
 
-  const grid = Array.from({ length: 1000 }).map(a =>
-    Array.from({ length: 1000 }, () => 0)
+  const grid = Array.from({ length: gridSize }).map(a =>
+    Array.from({ length: gridSize }, () => 0)
   )
 
   const splitCoordinates = coordinates =>
     coordinates.split(',').map(c => parseInt(c, 10))
 
+  const makeDiagonalLine = (isUpSlope, startYIndex, startX, endX) => {
+    const lineLength = Math.abs(startX - endX) + 1
+    const startXIndex = startX < endX ? startX : endX
+    let x = startXIndex
+    let y = startYIndex
+
+    while (x < startXIndex + lineLength) {
+      grid[y][x]++
+      x++
+      y = isUpSlope ? y - 1 : y + 1
+    }
+  }
+
   input.forEach(line => {
     const [startingCoordinates, endingCoordinates] = line.split(' -> ')
-
     const [startX, startY] = splitCoordinates(startingCoordinates)
     const [endX, endY] = splitCoordinates(endingCoordinates)
 
     if (startX === endX) {
       // vertical
-      const lineLength = Math.abs(endY - startY) + 1
       const startIndex = startY < endY ? startY : endY
-      for (i = startIndex; i < startIndex + lineLength; i++) {
+
+      for (i = startIndex; i < startIndex + Math.abs(endY - startY) + 1; i++) {
         grid[i][startX]++
       }
     } else if (startY === endY) {
       // horizontal
-      const lineLength = Math.abs(startX - endX) + 1
       const startIndex = startX < endX ? startX : endX
-      for (i = startIndex; i < startIndex + lineLength; i++) {
+
+      for (i = startIndex; i < startIndex + Math.abs(startX - endX) + 1; i++) {
         grid[startY][i]++
       }
     } else if (includeDiagonals) {
-      // diagonal
-      const lineLength = Math.abs(startX - endX) + 1
-      const startXIndex = startX < endX ? startX : endX
-      let x = startXIndex
-
       if (
         (startY > endY && startX < endX) ||
         (startY < endY && startX > endX)
       ) {
         // up slope
-        const startYIndex = startY < endY ? endY : startY
-        let y = startYIndex
-
-        while (x < startXIndex + lineLength) {
-          grid[y][x]++
-          x++
-          y--
-        }
+        makeDiagonalLine(true, startY < endY ? endY : startY, startX, endX)
       } else {
         // down slope
-        const startYIndex = startY < endY ? startY : endY
-        let y = startYIndex
-
-        while (x < startXIndex + lineLength) {
-          grid[y][x]++
-          x++
-          y++
-        }
+        makeDiagonalLine(false, startY < endY ? startY : endY, startX, endX)
       }
     }
   })
@@ -67,21 +60,23 @@ const go = (fileName, includeDiagonals) => {
   return grid.flat().reduce((acc, cur) => acc + (cur >= 2 ? 1 : 0), 0)
 }
 
-
 const process = (part, expectedAnswer, includeDiagonals) => {
-  const sampleAnswer = go('./day_05/sample_input.txt', includeDiagonals)
+  const sampleAnswer = mapHydrothermalVents(
+    './day_05/sample_input.txt',
+    10,
+    includeDiagonals
+  )
 
   console.log(`part ${part} sample answer`, sampleAnswer)
   if (sampleAnswer !== expectedAnswer) {
-    throw new Error(`${part} one sample answer should be ${expectedAnswer}`)
+    throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
   }
 
   console.log(
     `part ${part} real answer`,
-    go('./day_05/input.txt', includeDiagonals)
+    mapHydrothermalVents('./day_05/input.txt', 1000, includeDiagonals)
   )
 }
 
-process('a', 5, false)
-process('b', 12, true)
-
+process('A', 5, false)
+process('B', 12, true)
