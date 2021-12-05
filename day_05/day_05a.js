3ef67695292b85a58837f171a7eff6b854340b89
andymueller85 <andymueller85@gmail.com>
2021-12-05 11:39:42 -0500

    day 5 add some verification code

--- day_05/day_05a.js
+++ day_05/day_05a.js
@@ -1,43 +1,47 @@
-const input = require('fs')
-  .readFileSync('./day_05/input.txt', 'utf8')
-  .split('\n')
-  .filter(d => d)
+const go = fileName => {
+  const input = require('fs')
+    .readFileSync(fileName, 'utf8')
+    .split('\n')
+    .filter(d => d)
 
-const grid = Array.from({ length: 1000 }).map(a =>
-  Array.from({ length: 1000 }, () => 0)
-)
+  const grid = Array.from({ length: 1000 }).map(a =>
+    Array.from({ length: 1000 }, () => 0)
+  )
 
-const splitCoordinates = coordinates =>
-  coordinates.split(',').map(c => parseInt(c, 10))
+  const splitCoordinates = coordinates =>
+    coordinates.split(',').map(c => parseInt(c, 10))
 
-input.forEach(line => {
-  const [startingCoordinates, endingCoordinates] = line.split(' -> ')
+  input.forEach(line => {
+    const [startingCoordinates, endingCoordinates] = line.split(' -> ')
 
-  const [startX, startY] = splitCoordinates(startingCoordinates)
-  const [endX, endY] = splitCoordinates(endingCoordinates)
+    const [startX, startY] = splitCoordinates(startingCoordinates)
+    const [endX, endY] = splitCoordinates(endingCoordinates)
 
-  if (startX === endX) {
-    // vertical
-    const lineLength = Math.abs(endY - startY) + 1
-    const startIndex = startY < endY ? startY : endY
+    if (startX === endX) {
+      // vertical
+      const lineLength = Math.abs(endY - startY) + 1
+      const startIndex = startY < endY ? startY : endY
 
-    for (i = startIndex; i < startIndex + lineLength; i++) {
-      grid[i][startX]++
+      for (i = startIndex; i < startIndex + lineLength; i++) {
+        grid[i][startX]++
+      }
     }
-  }
-  if (startY === endY) {
-    // horizontal
-    const lineLength = Math.abs(startX - endX) + 1
-    const startIndex = startX < endX ? startX : endX
+    if (startY === endY) {
+      // horizontal
+      const lineLength = Math.abs(startX - endX) + 1
+      const startIndex = startX < endX ? startX : endX
 
-    for (i = startIndex; i < startIndex + lineLength; i++) {
-      grid[startY][i]++
+      for (i = startIndex; i < startIndex + lineLength; i++) {
+        grid[startY][i]++
+      }
     }
-  }
-})
+  })
 
-const atLeastTwoLinesOverlapCount = grid
-  .flat()
-  .reduce((acc, cur) => acc + (cur >= 2 ? 1 : 0), 0)
+  return grid.flat().reduce((acc, cur) => acc + (cur >= 2 ? 1 : 0), 0)
+}
 
-console.log('answer', atLeastTwoLinesOverlapCount)
+const sampleAnswer = go('./day_05/sample_input.txt')
+console.log('sample answer', sampleAnswer)
+if (sampleAnswer !== 5) throw new Error("this ain't it, jack")
+
+console.log('real answer', go('./day_05/input.txt'))
--- day_05/day_05b.js
+++ day_05/day_05b.js
@@ -1,68 +1,75 @@
-const input = require('fs')
-  .readFileSync('./day_05/input.txt', 'utf8')
-  .split('\n')
-  .filter(d => d)
+const go = fileName => {
+  const input = require('fs')
+    .readFileSync(fileName, 'utf8')
+    .split('\n')
+    .filter(d => d)
 
-const grid = Array.from({ length: 1000 }).map(a =>
-  Array.from({ length: 1000 }, () => 0)
-)
+  const grid = Array.from({ length: 1000 }).map(a =>
+    Array.from({ length: 1000 }, () => 0)
+  )
 
-const splitCoordinates = coordinates =>
-  coordinates.split(',').map(c => parseInt(c, 10))
+  const splitCoordinates = coordinates =>
+    coordinates.split(',').map(c => parseInt(c, 10))
 
-input.forEach(line => {
-  const [startingCoordinates, endingCoordinates] = line.split(' -> ')
+  input.forEach(line => {
+    const [startingCoordinates, endingCoordinates] = line.split(' -> ')
 
-  const [startX, startY] = splitCoordinates(startingCoordinates)
-  const [endX, endY] = splitCoordinates(endingCoordinates)
+    const [startX, startY] = splitCoordinates(startingCoordinates)
+    const [endX, endY] = splitCoordinates(endingCoordinates)
 
-  if (startX === endX) {
-    // vertical
-    const lineLength = Math.abs(endY - startY) + 1
-    const startIndex = startY < endY ? startY : endY
-    for (i = startIndex; i < startIndex + lineLength; i++) {
-      grid[i][startX]++
-    }
-  } else if (startY === endY) {
-    // horizontal
-    const lineLength = Math.abs(startX - endX) + 1
-    const startIndex = startX < endX ? startX : endX
-    for (i = startIndex; i < startIndex + lineLength; i++) {
-      grid[startY][i]++
-    }
-  } else {
-    // diagonal
-    const lineLength = Math.abs(startX - endX) + 1
-    if ((startY > endY && startX < endX) || (startY < endY && startX > endX)) {
-      // up slope
-      const startXIndex = startX < endX ? startX : endX
-      const startYIndex = startY < endY ? endY : startY
-      let x = startXIndex
-      let y = startYIndex
-      while (x < startXIndex + lineLength) {
-        grid[y][x]++
-        x++
-        y--
+    if (startX === endX) {
+      // vertical
+      const lineLength = Math.abs(endY - startY) + 1
+      const startIndex = startY < endY ? startY : endY
+      for (i = startIndex; i < startIndex + lineLength; i++) {
+        grid[i][startX]++
+      }
+    } else if (startY === endY) {
+      // horizontal
+      const lineLength = Math.abs(startX - endX) + 1
+      const startIndex = startX < endX ? startX : endX
+      for (i = startIndex; i < startIndex + lineLength; i++) {
+        grid[startY][i]++
       }
     } else {
-      // down slope
-      const startXIndex = startX < endX ? startX : endX
-      const startYIndex = startY < endY ? startY : endY
+      // diagonal
+      const lineLength = Math.abs(startX - endX) + 1
+      if (
+        (startY > endY && startX < endX) ||
+        (startY < endY && startX > endX)
+      ) {
+        // up slope
+        const startXIndex = startX < endX ? startX : endX
+        const startYIndex = startY < endY ? endY : startY
+        let x = startXIndex
+        let y = startYIndex
+        while (x < startXIndex + lineLength) {
+          grid[y][x]++
+          x++
+          y--
+        }
+      } else {
+        // down slope
+        const startXIndex = startX < endX ? startX : endX
+        const startYIndex = startY < endY ? startY : endY
 
-      let x = startXIndex
-      let y = startYIndex
+        let x = startXIndex
+        let y = startYIndex
 
-      while (x < startXIndex + lineLength) {
-        grid[y][x]++
-        x++
-        y++
+        while (x < startXIndex + lineLength) {
+          grid[y][x]++
+          x++
+          y++
+        }
       }
     }
-  }
-})
+  })
 
-const atLeastTwoLinesOverlapCount = grid
-  .flat()
-  .reduce((acc, cur) => acc + (cur >= 2 ? 1 : 0), 0)
+  return grid.flat().reduce((acc, cur) => acc + (cur >= 2 ? 1 : 0), 0)
+}
 
-console.log('answer', atLeastTwoLinesOverlapCount)
+const sampleAnswer = go('./day_05/sample_input.txt')
+console.log('sample answer', sampleAnswer)
+if (sampleAnswer !== 12) throw new Error("this ain't it, jack")
+
+console.log('real answer', go('./day_05/input.txt'))
