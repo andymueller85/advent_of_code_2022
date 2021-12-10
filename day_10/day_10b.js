14febe70484aecbe06d51cafdd2ada90ab3319b6
Andy Mueller <andymueller85@gmail.com>
2021-12-10 07:59:21 -0500

    day 10 cleanup

--- day_10/day_10b.js
+++ day_10/day_10b.js
@@ -1,23 +1,8 @@
-const getIncompleteRowScore = (fileName, crabCostFn) => {
-  const input = require('fs')
-    .readFileSync(fileName, 'utf8')
-    .split('\n')
-    .filter(d => d)
-    .map(r => r.split(''))
+const isMatch = (opener, closer) =>
+  openers.findIndex(o => o === opener) === closers.findIndex(c => c === closer)
 
-  const openers = ['(', '[', '{', '<']
-  const closers = [')', ']', '}', '>']
-
-  const isMatch = (opener, closer) =>
-    openers.findIndex(o => o === opener) ===
-    closers.findIndex(c => c === closer)
-
-  const getScore = char => closers.findIndex(c => c === char) + 1
-
-  const scoreCompletionString = str =>
-    str.split('').reduce((acc, cur) => acc * 5 + getScore(cur), 0)
-
-  const incompleteLines = input.filter(i => {
+const getIncompleteLines = lines =>
+  lines.filter(i => {
     const stack = []
     return !i.find(char => {
       if (openers.includes(char)) {
@@ -28,26 +13,40 @@ const getIncompleteRowScore = (fileName, crabCostFn) => {
     })
   })
 
-  const completionStrings = incompleteLines.map(l => {
-    const stack = []
+const getScore = char => closers.findIndex(c => c === char) + 1
 
-    l.forEach(char => {
-      if (openers.includes(char)) {
-        stack.push(char)
-      } else {
-        stack.pop()
-      }
+const scoreCompletionString = str =>
+  str.split('').reduce((acc, cur) => acc * 5 + getScore(cur), 0)
+
+const openers = ['(', '[', '{', '<']
+const closers = [')', ']', '}', '>']
+
+const getIncompleteRowScore = (fileName, crabCostFn) => {
+  const input = require('fs')
+    .readFileSync(fileName, 'utf8')
+    .split('\n')
+    .filter(d => d)
+    .map(r => r.split(''))
+
+  const scores = getIncompleteLines(input)
+    .map(l => {
+      const stack = []
+
+      l.forEach(char => {
+        if (openers.includes(char)) {
+          stack.push(char)
+        } else {
+          stack.pop()
+        }
+      })
+
+      return stack
+        .reverse()
+        .reduce(
+          (acc, cur) => acc + closers[openers.findIndex(o => o === cur)],
+          ''
+        )
     })
-
-    return stack
-      .reverse()
-      .reduce(
-        (acc, cur) => acc + closers[openers.findIndex(o => o === cur)],
-        ''
-      )
-  })
-
-  const scores = completionStrings
     .map(scoreCompletionString)
     .sort((a, b) => a - b)
 
