86abea62b2183090c13646ff2cb75ad78231259a
andymueller85 <andymueller85@gmail.com>
2021-12-14 20:13:10 -0500

    cleanup

--- day_14/day_14b.js
+++ day_14/day_14b.js
@@ -30,8 +30,8 @@ const polymerize = (fileName, days) => {
 
   // for each iteration, apply the rule. if it creates a new instance of a rule, increase the count.
   Array.from({ length: days }).forEach(() => {
-    let dayCharCounts = {}
-    let newRuleMap = pairs.reduce((acc, cur) => ({ ...acc, [cur]: 0 }), {})
+    const dayCharCounts = {}
+    const newRuleMap = pairs.reduce((acc, cur) => ({ ...acc, [cur]: 0 }), {})
 
     rules.forEach(([pair, char]) => {
       dayCharCounts[char] = (dayCharCounts[char] ?? 0) + ruleMap[pair]
