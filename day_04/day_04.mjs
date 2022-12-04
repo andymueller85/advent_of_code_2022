e5278bd98f6bb07b51110c16fbc019b8dea1c180
andymueller85 <andymueller85@gmail.com>
2022-12-04 08:59:07 -0500

    moar DRY

--- day_04/day_04.mjs
+++ day_04/day_04.mjs
@@ -6,48 +6,38 @@ const parsePairs = fileName =>
     .split(/\r?\n/)
     .filter(d => d)
 
-const getBoundaries = range => range.split('-').map(l => parseInt(l, 10))
+const parseBoundaries = range => range.split('-').map(l => parseInt(l, 10))
 
-const getElves = pair => {
+const parseElves = pair => {
   const [e1Range, e2Range] = pair.split(',')
-  const [e1Lower, e1Upper] = getBoundaries(e1Range)
-  const [e2Lower, e2Upper] = getBoundaries(e2Range)
+  const [e1Lower, e1Upper] = parseBoundaries(e1Range)
+  const [e2Lower, e2Upper] = parseBoundaries(e2Range)
 
   return { e1: { lower: e1Lower, upper: e1Upper }, e2: { lower: e2Lower, upper: e2Upper } }
 }
 
-const cleanupCampA = fileName =>
-  parsePairs(fileName).reduce((acc, cur) => {
-    const { e1, e2 } = getElves(cur)
-    const subset =
-      (e1.lower <= e2.lower && e1.upper >= e2.upper) ||
-      (e2.lower <= e1.lower && e2.upper >= e1.upper)
+const partATest = ({ e1, e2 }) =>
+  (e1.lower <= e2.lower && e1.upper >= e2.upper) || (e2.lower <= e1.lower && e2.upper >= e1.upper)
 
-    return acc + (subset ? 1 : 0)
-  }, 0)
+const partBTest = ({ e1, e2 }) =>
+  (e1.lower >= e2.lower && e1.lower <= e2.upper) ||
+  (e1.upper >= e2.lower && e1.upper <= e2.upper) ||
+  (e2.lower >= e1.lower && e2.lower <= e1.upper) ||
+  (e2.upper >= e1.lower && e2.upper <= e1.upper)
 
-const cleanupCampB = fileName =>
-  parsePairs(fileName).reduce((acc, cur) => {
-    const { e1, e2 } = getElves(cur)
-    const overlap =
-      (e1.lower >= e2.lower && e1.lower <= e2.upper) ||
-      (e1.upper >= e2.lower && e1.upper <= e2.upper) ||
-      (e2.lower >= e1.lower && e2.lower <= e1.upper) ||
-      (e2.upper >= e1.lower && e2.upper <= e1.upper)
+const cleanupCamp = (fileName, testFn) =>
+  parsePairs(fileName).reduce((acc, cur) => acc + (testFn(parseElves(cur)) ? 1 : 0), 0)
 
-    return acc + (overlap ? 1 : 0)
-  }, 0)
-
-const process = (part, expectedAnswer, fn) => {
-  const sampleAnswer = fn('./day_04/sample_input.txt')
+const process = (part, expectedAnswer, testFn) => {
+  const sampleAnswer = cleanupCamp('./day_04/sample_input.txt', testFn)
 
   console.log(`part ${part} sample answer`, sampleAnswer)
   if (sampleAnswer !== expectedAnswer) {
     throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
   }
 
-  console.log(`part ${part} real answer`, fn('./day_04/input.txt'))
+  console.log(`part ${part} real answer`, cleanupCamp('./day_04/input.txt', testFn))
 }
 
-process('A', 2, cleanupCampA)
-process('B', 4, cleanupCampB)
+process('A', 2, partATest)
+process('B', 4, partBTest)
