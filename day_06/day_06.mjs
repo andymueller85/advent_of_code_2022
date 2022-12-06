a9bb7bd63a94e249faee3ef7f96b51f9d9e70cb4
Andy Mueller <andymueller85@gmail.com>
2022-12-06 13:55:54 -0500

    day 6

---
+++ day_06/day_06.mjs
@@ -0,0 +1,22 @@
+import * as fs from 'fs'
+
+const arrValuesUnique = arr => new Set(arr).size === arr.length
+const findMarker = (fileName, charCount) =>
+  fs
+    .readFileSync(fileName, 'utf8')
+    .split('')
+    .findIndex((_, i, arr) => i >= charCount && arrValuesUnique(arr.slice(i - charCount, i)))
+
+const process = (part, expectedAnswer, charCount) => {
+  const sampleAnswer = findMarker('./day_06/sample_input.txt', charCount)
+
+  console.log(`part ${part} sample answer`, sampleAnswer)
+  if (sampleAnswer !== expectedAnswer) {
+    throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
+  }
+
+  console.log(`part ${part} real answer`, findMarker('./day_06/input.txt', charCount))
+}
+
+process('A', 7, 4)
+process('A', 19, 14)
