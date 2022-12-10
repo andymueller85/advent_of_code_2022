6a2aa20bc508d4f29801fad635347c00c13d8e9b
andymueller85 <andymueller85@gmail.com>
2022-12-10 11:53:08 -0500

    day 10b

--- day_10/day_10.mjs
+++ day_10/day_10.mjs
@@ -8,7 +8,7 @@ const parseInput = fileName =>
     .map(i => i.split(' '))
     .map(([instr, val]) => [instr, parseInt(val)])
 
-const execute = fileName => {
+const partA = fileName => {
   const input = parseInput(fileName)
 
   let cycle = 1
@@ -41,15 +41,60 @@ const execute = fileName => {
   return total
 }
 
-const process = (part, expectedAnswer) => {
-  const sampleAnswer = execute('./day_10/sample_input.txt')
+const partB = fileName => {
+  const WIDTH = 40
+  const HEIGHT = 6
+
+  const input = parseInput(fileName)
+  const screen = Array.from({ length: HEIGHT }, () => Array.from({ length: WIDTH }, () => '.'))
+  let sprite = 1
+  let pixel = [0, 0]
+
+  const advanceCycle = () => {
+    if (pixel[1] < WIDTH - 1) {
+      pixel[1]++
+    } else {
+      pixel[0]++
+      pixel[1] = 0
+    }
+  }
+
+  const renderPixel = () => {
+    if ([sprite - 1, sprite, sprite + 1].includes(pixel[1])) {
+      screen[pixel[0]][pixel[1]] = '#'
+    }
+  }
+
+  input.forEach(([instr, val]) => {
+    if (instr === 'noop') {
+      renderPixel()
+      advanceCycle()
+    } else if (instr === 'addx') {
+      Array.from({ length: 2 }).forEach(() => {
+        renderPixel()
+        advanceCycle()
+      })
+
+      sprite += val
+    }
+  })
+
+  screen.forEach(l => console.log(l.join('')))
+}
+
+const process = (part, expectedAnswer, fn) => {
+  const sampleAnswer = fn('./day_10/sample_input.txt')
 
   console.log(`part ${part} sample answer`, sampleAnswer)
   if (sampleAnswer !== expectedAnswer) {
     throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
   }
 
-  console.log(`part ${part} real answer`, execute('./day_10/input.txt'))
+  console.log(`part ${part} real answer`, fn('./day_10/input.txt'))
 }
 
-process('A', 13140)
+process('A', 13140, partA)
+console.log('\n\n********** PART B SAMPLE **********')
+partB('./day_10/sample_input.txt')
+console.log('\n\n********** PART B REAL ************')
+partB('./day_10/input.txt')
