05b36246758f0f74b4f0c5b575a7dd120653eeb5
Andy Mueller <andymueller85@gmail.com>
2022-12-13 16:33:17 -0500

    day 13a

---
+++ day_13/day_13.mjs
@@ -0,0 +1,44 @@
+import * as fs from 'fs'
+
+// returns negative if a < b, 0 if a == b, positive if a > b.
+const compare = (left, right) => {
+  if (typeof left === 'number' && typeof right === 'number') {
+    return left - right
+  } else if (Array.isArray(left) && typeof right === 'number') {
+    return compare(left, [right])
+  } else if (typeof left === 'number' && Array.isArray(right)) {
+    return compare([left], right)
+  } else if (Array.isArray(left) && Array.isArray(right)) {
+    for (let i = 0; i < Math.min(left.length, right.length); i++) {
+      const c = compare(left[i], right[i])
+      if (c !== 0) return c
+    }
+    return left.length - right.length
+  }
+}
+
+const main = fileName => {
+  return fs
+    .readFileSync(fileName, 'utf8')
+    .split(/\r?\n\r?\n/)
+    .map(pair =>
+      pair
+        .split(/\r?\n/)
+        .filter(d => d)
+        .map(packet => JSON.parse(packet))
+    )
+    .reduce((acc, [left, right], i) => acc + (compare(left, right) < 0 ? i + 1 : 0), 0)
+}
+
+const process = (part, expectedAnswer) => {
+  const sampleAnswer = main('./day_13/sample_input.txt')
+
+  console.log(`part ${part} sample answer`, sampleAnswer)
+  if (sampleAnswer !== expectedAnswer) {
+    throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
+  }
+
+  console.log(`part ${part} real answer`, main('./day_13/input.txt'))
+}
+
+process('A', 13)
