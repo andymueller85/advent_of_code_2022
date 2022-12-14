af37078dda1d9eb5d0e4104806f1bd0784ccccab
Andy Mueller <andymueller85@gmail.com>
2022-12-14 09:40:40 -0500

    day 14b

--- day_14/day_14.mjs
+++ day_14/day_14.mjs
@@ -4,15 +4,17 @@ const AIR = '.'
 const ROCK = '#'
 const SAND = 'o'
 
-const partA = fileName => {
-  const input = fs
+const processInput = fileName =>
+  fs
     .readFileSync(fileName, 'utf8')
     .split(/\r?\n/)
     .filter(d => d)
     .map(l => l.split(' -> ').map(c => c.split(',')))
 
-  const grid = Array.from({ length: GRID_SIZE }, () => Array.from({ length: GRID_SIZE }, () => '.'))
+const createGrid = () =>
+  Array.from({ length: GRID_SIZE }, () => Array.from({ length: GRID_SIZE }, () => '.'))
 
+const placeRocks = (input, grid, floor) => {
   input.forEach(line => {
     line.forEach(([x, y], i) => {
       if (line[i + 1]) {
@@ -35,9 +37,24 @@ const partA = fileName => {
     })
   })
 
-  let keepGoing = true
-  let sandCount = 0
+  if (floor) {
+    const floorIndex = grid.reduce((acc, cur, i) => {
+      if (cur.includes(ROCK)) {
+        return i + 2
+      }
+      return acc
+    }, 0)
 
+    console.log(floorIndex)
+    grid[floorIndex] = grid[floorIndex].map(() => ROCK)
+  }
+
+  return grid
+}
+
+const partA = grid => {
+  let sandCount = 0
+  let keepGoing = true
   while (keepGoing) {
     let sandX = 0
     let sandY = 500
@@ -71,15 +88,60 @@ const partA = fileName => {
   return sandCount
 }
 
-const process = (part, expectedAnswer, fn) => {
-  const sampleAnswer = fn('./day_14/sample_input.txt')
+const partB = grid => {
+  let sandCount = 0
+  let keepGoing = true
+  while (grid[0][500] === AIR) {
+    let sandX = 0
+    let sandY = 500
+    let falling = true
+    while (falling) {
+      if (sandX === GRID_SIZE - 1) {
+        falling = false
+        keepGoing = false
+      } else if (grid[sandX + 1][sandY] === AIR) {
+        // fall straight down
+        sandX++
+      } else if ([ROCK, SAND].includes(grid[sandX + 1][sandY])) {
+        if (grid[sandX + 1][sandY - 1] === AIR) {
+          // down & left
+          sandY--
+          sandX++
+        } else if (grid[sandX + 1][sandY + 1] === AIR) {
+          // down & right
+          sandY++
+          sandX++
+        } else {
+          // come to rest
+          grid[sandX][sandY] = SAND
+          falling = false
+          sandCount++
+        }
+      }
+    }
+  }
+
+  return sandCount
+}
+
+const main = (fileName, fn, floor) => {
+  const input = processInput(fileName)
+  const grid = createGrid()
+  const gridWithRocks = placeRocks(input, grid, floor)
+
+  return fn(gridWithRocks)
+}
+
+const process = (part, expectedAnswer, fn, floor) => {
+  const sampleAnswer = main('./day_14/sample_input.txt', fn, floor)
 
   console.log(`part ${part} sample answer`, sampleAnswer)
   if (sampleAnswer !== expectedAnswer) {
     throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
   }
 
-  console.log(`part ${part} real answer`, fn('./day_14/input.txt'))
+  console.log(`part ${part} real answer`, main('./day_14/input.txt', fn, floor))
 }
 
-process('A', 24, partA)
+process('A', 24, partA, false)
+process('B', 93, partB, true)
