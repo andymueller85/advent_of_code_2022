cb182d615f6d3ae9c2fc1010f3325dc1c964d568
Andy Mueller <andymueller85@gmail.com>
2022-12-14 10:28:56 -0500

    DRY up day 14

--- day_14/day_14.mjs
+++ day_14/day_14.mjs
@@ -38,31 +38,27 @@ const placeRocks = (input, grid, floor) => {
   })
 
   if (floor) {
-    const floorIndex = grid.reduce((acc, cur, i) => {
-      if (cur.includes(ROCK)) {
-        return i + 2
-      }
-      return acc
-    }, 0)
-
-    console.log(floorIndex)
+    const floorIndex = grid.reduce((acc, cur, i) => (cur.includes(ROCK) ? i + 2 : acc), 0)
     grid[floorIndex] = grid[floorIndex].map(() => ROCK)
   }
 
   return grid
 }
 
-const partA = grid => {
+const continueFnA = (_, sandX, limit) => sandX < limit
+const continueFnB = entrance => entrance === AIR
+
+const dropSand = (grid, continueFn) => {
   let sandCount = 0
-  let keepGoing = true
-  while (keepGoing) {
-    let sandX = 0
-    let sandY = 500
+  let sandX = 0
+  let sandY = 500
+  while (continueFn(grid[0][500], sandX, GRID_SIZE - 1)) {
+    sandX = 0
+    sandY = 500
     let falling = true
     while (falling) {
       if (sandX === GRID_SIZE - 1) {
         falling = false
-        keepGoing = false
       } else if (grid[sandX + 1][sandY] === AIR) {
         // fall straight down
         sandX++
@@ -88,60 +84,24 @@ const partA = grid => {
   return sandCount
 }
 
-const partB = grid => {
-  let sandCount = 0
-  let keepGoing = true
-  while (grid[0][500] === AIR) {
-    let sandX = 0
-    let sandY = 500
-    let falling = true
-    while (falling) {
-      if (sandX === GRID_SIZE - 1) {
-        falling = false
-        keepGoing = false
-      } else if (grid[sandX + 1][sandY] === AIR) {
-        // fall straight down
-        sandX++
-      } else if ([ROCK, SAND].includes(grid[sandX + 1][sandY])) {
-        if (grid[sandX + 1][sandY - 1] === AIR) {
-          // down & left
-          sandY--
-          sandX++
-        } else if (grid[sandX + 1][sandY + 1] === AIR) {
-          // down & right
-          sandY++
-          sandX++
-        } else {
-          // come to rest
-          grid[sandX][sandY] = SAND
-          falling = false
-          sandCount++
-        }
-      }
-    }
-  }
-
-  return sandCount
-}
-
-const main = (fileName, fn, floor) => {
+const main = (fileName, continueFn, floor) => {
   const input = processInput(fileName)
   const grid = createGrid()
   const gridWithRocks = placeRocks(input, grid, floor)
 
-  return fn(gridWithRocks)
+  return dropSand(gridWithRocks, continueFn)
 }
 
-const process = (part, expectedAnswer, fn, floor) => {
-  const sampleAnswer = main('./day_14/sample_input.txt', fn, floor)
+const process = (part, expectedAnswer, continueFn, floor) => {
+  const sampleAnswer = main('./day_14/sample_input.txt', continueFn, floor)
 
   console.log(`part ${part} sample answer`, sampleAnswer)
   if (sampleAnswer !== expectedAnswer) {
     throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
   }
 
-  console.log(`part ${part} real answer`, main('./day_14/input.txt', fn, floor))
+  console.log(`part ${part} real answer`, main('./day_14/input.txt', continueFn, floor))
 }
 
-process('A', 24, partA, false)
-process('B', 93, partB, true)
+process('A', 24, continueFnA, false)
+process('B', 93, continueFnB, true)
